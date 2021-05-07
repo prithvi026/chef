28e81b13b13447124f0a7b3dd6f8a63ea143d5d7
root <root@ip-172-31-9-191.ap-south-1.compute.internal>
2021-05-07 14:24:34 +0000

    Creating attributes in chef

---
+++ attribute-cookbook/README.md
@@ -0,0 +1,4 @@
+# attribute-cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ attribute-cookbook/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: attribute-cookbook
+# Recipe:: default
+#
+# Copyright:: 2021, The Authors, All Rights Reserved.
